db035a6a49ee8335a498f4557a5d60da8077c06a
Govind252005 <govind252006@gmail.com>
2026-01-19 22:21:35 +0530

    Fix Razorpay initialization for deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,20 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import validator from "validator";
+import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
-import { v2 as cloudinary } from 'cloudinary'
-import stripe from "stripe";
-import razorpay from 'razorpay';
 
-// Gateway Initialize
-const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
+// Initialize Razorpay only if keys are provided
+let razorpayInstance = null;
+if (process.env.RAZORPAY_KEY_ID && process.env.RAZORPAY_KEY_SECRET) {
+  const razorpay = await import('razorpay');
+  razorpayInstance = new razorpay.default({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
-})
+  });
+}
 
 // API to register user
 const registerUser = async (req, res) => {
